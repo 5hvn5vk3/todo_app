8885c939a90cbbbd3547840cc2c9ca4e0f5a4755
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-12-23 15:19:42 +0900

    feat: CreateTodo

--- app/models/base.go
+++ app/models/base.go
@@ -35,7 +35,7 @@ func init() {
 
 	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
 	    id INTEGER PRIMARY KEY AUTOINCREMENT,
-		content TEXT
+		content TEXT,
 		user_id INTEGER,
 		created_at DATETIME)`, tableNameTodo)
 	Db.Exec(cmdT)
--- main.go
+++ main.go
@@ -20,32 +20,45 @@ func main() {
 	u.CreateUser()
 	log.Println("ユーザー作成完了")
 
+	// // 作成したユーザーを取得
+	// user, _ := models.GetUser(1)
+	// log.Println("取得したユーザー:", user)
+
+	// // ユーザー情報を更新
+	// user.Name = "testName2"
+	// user.Email = "test2@example.com"
+	// user.UpdateUser()
+	// log.Println("ユーザー更新完了")
+
+	// // 更新後のユーザーを取得
+	// user, _ = models.GetUser(1)
+	// log.Println("更新後のユーザー:", user)
+
+	// // ユーザーを削除
+	// err := user.DeleteUser()
+	// if err != nil {
+	// 	log.Fatalln("ユーザー削除失敗:", err)
+	// }
+	// log.Println("ユーザー削除完了")
+
+	// // 削除後のユーザー取得を試みる
+	// _, err = models.GetUser(1)
+	// if err != nil {
+	// 	log.Println("削除後のユーザー取得失敗（予期された動作）:", err)
+	// } else {
+	// 	log.Println("削除後のユーザーがまだ存在しています（予期しない動作）")
+	// }
+
 	// 作成したユーザーを取得
-	user, _ := models.GetUser(1)
+	user, err := models.GetUser(1)
+	if err != nil {
+		log.Fatalln("ユーザー取得失敗:", err)
+	}
 	log.Println("取得したユーザー:", user)
 
-	// ユーザー情報を更新
-	user.Name = "testName2"
-	user.Email = "test2@example.com"
-	user.UpdateUser()
-	log.Println("ユーザー更新完了")
-
-	// 更新後のユーザーを取得
-	user, _ = models.GetUser(1)
-	log.Println("更新後のユーザー:", user)
-
-	// ユーザーを削除
-	err := user.DeleteUser()
+	err = user.CreateTodo("First Todo")
 	if err != nil {
-		log.Fatalln("ユーザー削除失敗:", err)
+		log.Fatalln("Todo作成失敗:", err)
 	}
-	log.Println("ユーザー削除完了")
-
-	// 削除後のユーザー取得を試みる
-	_, err = models.GetUser(1)
-	if err != nil {
-		log.Println("削除後のユーザー取得失敗（予期された動作）:", err)
-	} else {
-		log.Println("削除後のユーザーがまだ存在しています（予期しない動作）")
-	}
+	log.Println("Todo作成完了")
 }
