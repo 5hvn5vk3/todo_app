b276f473dc05ef14269f9a753451845b100a3331
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-12-24 12:51:19 +0900

    fix: 直前のコミットでコミットし忘れていたファイルのコミット

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,21 @@
 package controllers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
-func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+func generateHTML(w http.ResponseWriter, data any, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
 	}
-	t.Execute(w, "Hello")
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
+func top(w http.ResponseWriter, r *http.Request) {
+	generateHTML(w, "Hello", "layout", "top")
 }
