6918ae4da2f4032e7faa32d85ce64c8a039d6dcf
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-12-27 15:56:52 +0900

    fix: generateHTMLを宣言するファイルの修正

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,20 +1,8 @@
 package controllers
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
-
-func generateHTML(w http.ResponseWriter, data any, filenames ...string) {
-	var files []string
-	for _, file := range filenames {
-		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
-	}
-
-	templates := template.Must(template.ParseFiles(files...))
-	templates.ExecuteTemplate(w, "layout", data)
-}
 
 func top(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, "Hello", "layout", "public_navbar", "top")
