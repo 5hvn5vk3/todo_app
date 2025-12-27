c794f57933e23a89a339ba330f5089f4c1d5865a
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-12-27 16:30:33 +0900

    add: ログインしているかどうかで表示されるページが変わるようリダイレクトの設定を行った

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -9,7 +9,12 @@ import (
 func siginup(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		generateHTML(w, nil, "layout", "public_navbar", "signup")
+		_, err := session(w, r)
+		if err != nil {
+			generateHTML(w, nil, "layout", "public_navbar", "signup")
+		} else {
+			http.Redirect(w, r, "/todos", http.StatusFound)
+		}
 	case "POST":
 		err := r.ParseForm()
 		if err != nil {
@@ -28,7 +33,12 @@ func siginup(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "login")
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, nil, "layout", "public_navbar", "login")
+	} else {
+		http.Redirect(w, r, "/todos", http.StatusFound)
+	}
 }
 
 func authenticate(w http.ResponseWriter, r *http.Request) {
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"todo_app/app/models"
 	"todo_app/config"
 )
 
@@ -17,6 +18,17 @@ func generateHTML(w http.ResponseWriter, data any, filenames ...string) {
 	templates.ExecuteTemplate(w, "layout", data)
 }
 
+func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = models.Session{UUID: cookie.Value}
+		if ok, _ := sess.CheckSession(); !ok {
+			err = fmt.Errorf("Invalid Session")
+		}
+	}
+	return sess, err
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
@@ -24,5 +36,6 @@ func StartMainServer() error {
 	http.HandleFunc("/signup", siginup)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/todos", index)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
