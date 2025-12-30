cda6236364c6bdcee0d64ecd0cbe81a5530187f6
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-12-30 15:39:19 +0900

    chore: heroku版への変更といくつかの修正

--- app/models/base.go
+++ app/models/base.go
@@ -1,51 +1,38 @@
 package models
 
 import (
+	"crypto/sha1"
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"todo_app/config"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 var Db *sql.DB
 
 var err error
 
-const (
-	tableNameUser    = "users"
-	tableNameTodo    = "todos"
-	tableNameSession = "sessions"
-)
-
 func init() {
-	Db, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
+
+	url := os.Getenv("DATABASE_URL")
+	connection, _ := pq.ParseURL(url)
+	connection += "sslmode=require"
+	Db, err = sql.Open(config.Config.SQLDriver, connection)
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
 
-	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
-	    id INTEGER PRIMARY KEY AUTOINCREMENT,
-		uuid STRING NOT NULL UNIQUE,
-		name STRING,
-		email STRING,
-		password STRING,
-		created_at DATETIME)`, tableNameUser)
-	Db.Exec(cmdU)
+func createUUID() (uuidobj uuid.UUID) { // update: users.go から移動
+	uuidobj, _ = uuid.NewUUID()
+	return uuidobj
+}
 
-	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
-	    id INTEGER PRIMARY KEY AUTOINCREMENT,
-		content TEXT,
-		user_id INTEGER,
-		created_at DATETIME)`, tableNameTodo)
-	Db.Exec(cmdT)
-
-	cmdS := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
-	    id INTEGER PRIMARY KEY AUTOINCREMENT,
-		uuid STRING NOT NULL UNIQUE,
-		email STRING,
-		user_id INTEGER,
-		created_at DATETIME)`, tableNameSession)
-	Db.Exec(cmdS)
+func Encrypt(plaintext string) (cryptext string) { // update: users.go から移動
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return cryptext
 }
