a16e45cc2e66564de6f2caaa76752376c57adec3
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-12-31 15:39:01 +0900

    add: エラーログが出るようにした

--- app/models/base.go
+++ app/models/base.go
@@ -4,9 +4,11 @@ import (
 	"crypto/sha1"
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 var Db *sql.DB
@@ -15,7 +17,22 @@ var err error
 
 func init() {
 	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
 	Db, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	// データベース接続を確認
+	err = Db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	log.Println("Database connected successfully")
 }
 
 func createUUID() (uuidobj uuid.UUID) { // update: users.go から移動
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
-	fmt.Println(models.Db)
+	// データベース接続の確認
+	if models.Db == nil {
+		log.Fatal("Database connection is not initialized")
+	}
+	log.Println("Database instance:", models.Db)
 
-	controllers.StartMainServer()
+	// サーバー起動
+	log.Println("Starting server...")
+	if err := controllers.StartMainServer(); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
