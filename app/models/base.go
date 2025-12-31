dd85d556c45349920c056cec66b5c613ca6b76c1
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-12-31 12:40:57 +0900

    fix: DSN文字列の組み立てを修正

--- app/models/base.go
+++ app/models/base.go
@@ -20,7 +20,7 @@ func init() {
 
 	url := os.Getenv("DATABASE_URL")
 	connection, _ := pq.ParseURL(url)
-	connection += "sslmode=require"
+	connection = fmt.Sprintf("%s sslmode=require", connection)
 	Db, err = sql.Open(config.Config.SQLDriver, connection)
 	if err != nil {
 		log.Fatalln(err)
